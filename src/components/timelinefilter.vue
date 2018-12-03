573564ca9b6ef6d0d57668393209bed7e00b2af7
Mohammed Asif <mohd.asif55@gmail.com>
2018-12-02 21:19:24 -0800

    Linting

--- src/components/timelinefilter.vue
+++ src/components/timelinefilter.vue
@@ -1,49 +1,49 @@
 <template>
-    <div>                
+    <div class="timelinefilter-container">                
         <v-menu :close-on-content-click="false" v-model="showDatePickerMenu" :nudge-right="40" lazy 
             transition="scale-transition" offset-y full-width min-width="290px">
             <v-text-field slot="activator" v-model="date" label="Go around date" prepend-icon="event" readonly></v-text-field>
             <v-date-picker v-model="date" @input="showDatePickerMenu = false"></v-date-picker>
         </v-menu>
         <div v-for="event in this.$store.getters.events" :key="event.id">            
-            <v-switch v-model="selectedEvents" :label=event.name :value=event.id height="10px"></v-switch>
+            <v-switch v-model="selectedEvents" :label=event.name :value=event.id></v-switch>
         </div>
     </div>
 </template>
 
 <script lang='ts'>
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 @Component({
-    data:function(){
+    data: function() {
         return {
-            date: new Date().toISOString().substr(0, 10), 
+            date: new Date().toISOString().substr(0, 10),
             showDatePickerMenu: false,
-            selectedEvents: this.$store.getters.events.map(event => event.id)
+            selectedEvents: this.$store.getters.events.map((event) => event.id),
         };
     },
     watch: {
-        date: function (date) {
+        date: function(date) {
             this.triggerDateChanged(date);
         },
-        selectedEvents: function(selectedEvents){
+        selectedEvents: function(selectedEvents) {
             this.triggerEventSelectionChanged(selectedEvents);
-        }
-    }
+        },
+    },
 })
 export default class TimelineFilterComponent extends Vue {
-    constructor(){
-        super();
-        
+    private triggerDateChanged(date: string) {
     }
-    private triggerDateChanged(date){
-        console.log(date);
-    }
-    private triggerEventSelectionChanged(selectedEvents){
-        console.log(selectedEvents);
+    private triggerEventSelectionChanged(selectedEvents: []) {
     }
 }
 </script>
 
 <style lang='scss'>
+.timelinefilter{
+    &-container{
+        padding-left:20px;
+        padding-right:20px;
+    }
+}
 </style>
 
